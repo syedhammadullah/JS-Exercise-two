d474404b2c8eb4f6b7c2780c8275d43a84675bbb
Syed Hammad Ullah <154628306+syedhammadullah@users.noreply.github.com>
2024-03-07 00:53:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+//                                        Exercise 2
+
+/*
+
+Question # 1
+ 1)In the following code, "Mark" is a string. What is name (answer with 1 word)?
+
+var name = "Mark"; 
+
+ .)Answer = variable 
+
+Question # 2
+ 2)In one word, if it is enclosed in quotation marks, what is it?
+
+  .)Answer = string 
+
+Question # 3
+ 3)What is the keyword that declares a variable?
+ 
+ .)Answer = var 
+  
+Question # 4
+ 4)This statement...
+  userName = "buddy5000";
+  ...is correct only if the variable has already been _______.
+
+  .)Answer = declared 
+
+Question # 5
+ 5)Drag-and-drop. Holding down the left mouse button,
+  drag the pieces of code onto the screen to form a statement
+  that declares the variable punchline and assigns a value to it.
+
+  .)Answer = var punchline = "The accountant.";
+
+ Question # 6
+ 6)Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+
+  In a single statement, declare the variable nickname and assign the string "Satchmo" to it.  
+
+  .)Answer = var nickname = "Satchmo";
+Question # 7
+ 7)Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+
+   Declare the variable teamName.
+
+   .)Answer = var teamName;
+
+Question # 8
+ 8)In a single statement declare a variable and assign a string of your choice to it. 
+   (You've learned only one rule for naming variables so far,
+   so it's the only one I'll hold you to.)  
+   
+   .)Answer = var myName = "Syed Hammad Ullah";
+
+Question # 9
+ 9)Declare a variable. (You've learned only one rule for naming variables so far, 
+  so it's the only one I'll hold you to.)
+
+  .)Answer = var myName;
+
+Question # 10
+ 10)Assign a string of your choice to a variable of your choice that has already been declared. 
+ (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+ .)Answer = myName = "Wali";
+
+Question # 11
+ 11)This statement has already been coded.
+ var bestMan = "Charlie";
+ Assign the variable a new string.
+
+ .)Answer = bestMan = "Harry";
+
+Question # 12
+ 12)Declare a variable without defining it. Then, in a second statement, assign it a string.
+  (You've learned only one rule for naming variables so far,
+  so it's the only one I'll hold you to.) 
+
+  .)Answer = var myName;
+             myName = "Wali";
+
+Question # 13
+ 13)Time goal: 30 seconds. (The timer starts when you click in the answer field.)
+  In a single statement declare a variable of your choice and assign a string of your choice to it. You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.
+  (The timer starts when you click in the answer field.)
+
+  .)Answer = var myName = "Hammad"; (time 14 sec)
+
+Question # 14
+ 14)Time goal: 15 seconds. Declare a variable of your choice. (You've learned only one rule for naming variables so far,
+  so it's the only one I'll hold you to.) 
+  
+  .)Answer = var myName; (time 7 sec)
+
+Question # 15
+ 15)Time goal: 20 seconds. Assign a string of your choice to a variable of your choice that has already been declared. 
+  (You've learned only one rule for naming variables so far,
+  so it's the only one I'll hold you to.)  
+
+  .)Answer = myName = "Ahmed"; (time 14 sec)
+
+Question # 16
+ 16)Time goal: 20 seconds. This statement has already been coded.
+   var bestMan = "Charlie";
+   Assign the variable a new string of your choice.
+
+   .)Answer = bestMan = "Harry";  (time 17 sec)
+
+Question # 17
+ 17)Time goal: 35 seconds. Declare a variable of your choice without defining it. 
+  Then, in a second statement, assign it a string of your choice. 
+  (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)   
+
+  .)Answer = var Name;
+             Name = "Saylani";  (time 24 sec)
+
+Question # 18
+ 18)Time goal: 40 seconds. In the first statement declare a variable of your choice 
+  and assign it a string of your choice.
+  In the second statement change the value of the variable to a different string of your choice. 
+  
+  .)Answer = var lastName = "lastName";
+             lastName = "Ullah";  (time 28sec)
+
+Question # 19
+ 19)Live coding exercise: Declare a variable of your choice and assign it a string of your choice.
+  Then code an alert, specifying the variable, not the string, as the message.
+  Click the Run It button to run your code live. Type out the code, don't copy and paste it.
+  If you copy and paste it from the screen showing the correct code, it may not work.
+
+  .)Answer = var fullName = "Syed Hammad Ullah.";
+             alert(fullName);    
+
+Question # 20
+ 20)Live coding exercise: Declare a variable of your choice and assign it a 
+  string that's exactly the same as the variable name, 
+  but enclosed in quotation marks so JavaScript recognizes it as a string. 
+  Then code an alert, specifying the variable, not the string, as the message.
+  Click the Run It button to run your code live. 
+  Type out the code, don't copy and paste it.
+  If you copy and paste it from the screen showing the correct code, it may not work. 
+  
+  .)Answer = var oldName = "oldName";
+             alert(oldName);
+
+*/
+
+
+  
+
+ 
+
+
+
+  
+
+
+   
+
+
+  
